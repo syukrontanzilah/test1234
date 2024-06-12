236d51653b6bd6b88c73131f4dfcc35e4190f3bb
syukrontanzilah <syukrontanzilah@gmail.com>
2024-06-13 06:55:06 +0700

    test3

--- app/page.js
+++ app/page.js
@@ -13,24 +13,14 @@ export default function Home() {
 
   return (
     <div>
-      test aja ya
+      ini halaman depan
       <br/>
-      <input placeholder="Input disini ya"/>
-      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="green" class="bi bi-calendar" viewBox="0 0 16 16">
-        <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
-      </svg>
       <Image
         alt="person"
         src="https://cdn1-production-images-kly.akamaized.net/x7iMv-AbVp3elet3scpHjXjAw6U=/800x1066/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/2703595/original/033282200_1547455492-revalina-20190114-003-agus.jpg"
         width={250}
         height={250}
-/>
-      <Image
-        alt="person2"
-        src="/revalina.jpg"
-        width={300}
-        height={300}
-/>
+      />
     </div>
   );
 }

2d0c036ed57147df279e070824dd08fb3e79e92a
syukrontanzilah <syukrontanzilah@gmail.com>
2024-06-15 19:20:17 +0700

    contact me page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import {
+  MailIcon,
+  HomeIcon,
+  PhoneCall
+} from 'lucide-react';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section>
+      <div className='container mx-auto'>
+        {/* text and ilustration */}
+        <div className='grid xl:grid-cols-2 pt-12 xl:h-[480] mb-6 xl:mb-24'>
+          {/* text */}
+          <div className='flex flex-col justify-center'>
+            <div className='flex items-center gap-x-4 text-primary text-lg mb-4'>
+            <span className='w-[30px] h-[2px] bg-primary'></span>
+              Say Hello  👏          
+            </div>
+            <h1 className='h1 max-w-md mb-8'>Let's work together</h1>
+            <p className='subtitle max-w-[400px]'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi temporibus accusantium laborum. 
+            </p>
+          </div>
+          {/* ilustration */}
+          <div className='hidden xl:flex w-full bg-contact_illustration_light dark:bg-contact_illustration_dark bg-contain bg-top bg-no-repeat'></div>
+        </div>
+        {/* info text and form */}
+        <div className='grid xl:grid-cols-2 mb-24 xl:mb-32'>
+          {/* info text */}
+          <div className='flex flex-col gap-y-4 xl:gap-y-8 mb-12 xl:mb-24 text-base xl:text-lg'>
+            {/* mail */}
+            <div className='flex items-center gap-x-8'>
+              <MailIcon size={18} className='text-primary'/>
+              <div>syukrontanzilah@gmail.com</div>
+            </div>
+            {/* address */}
+            <div className='flex items-center gap-x-8'>
+              <HomeIcon size={18} className='text-primary'/>
+              <div>Cicurug, Kab. Sukabumi, Jawa Barat, Indonesia</div>
+            </div>
+            {/* mail */}
+            <div className='flex items-center gap-x-8'>
+              <PhoneCall size={18} className='text-primary'/>
+              <div>087823220303</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 

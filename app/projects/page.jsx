d5a9307dbc7616e04fbf0404793fa29c9986fbb9
syukrontanzilah <syukrontanzilah@gmail.com>
2024-06-15 18:18:45 +0700

    slicing project page

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
+'use client';
+import {useState} from 'react';
+import {
+  Tabs, 
+  TabsList, 
+  TabsContent, 
+  TabsTrigger,
+} from '@/components/ui/tabs'
+import ProjectCard from '@/components/ProjectCard';
+import { projectData } from '@/components/DummyData';
+
+// remove category duplicate
+const uniqueCategories = [
+  'all projects',
+  ...new Set(projectData.map((item)=> item.category))];
+
 
 const Projects = () => {
+  const [categories, setCategories] = useState(uniqueCategories);
+  const [category, setCategory] = useState('all projects')
+  // console.log('cat', category)
+  const filteredProjects = projectData.filter(project => {
+    return category === "all projects" ? project : project.category === category;
+  })
+
   return (
-    <div>Projects</div>
+    <section className="min-h-screen pt-12">
+      <div className="container mx-auto">
+        <h2 className='section-title mb-8 xl:mb-16 text-center mx-auto'>My Projects</h2>
+        {/* tabs */}
+        <Tabs defaultValue={category} className='mb-24 xl:mb-48'>
+          <TabsList className='w-full grid h-full md:grid-cols-4 lg:max-w-[640px] mb-12 mx-auto md:border dark:border-none'>
+            {categories.map((cat, i)=>{
+              return(
+                <TabsTrigger 
+                  onClick = {()=> setCategory(cat)}
+                  value={cat} 
+                  key={i}
+                  className='capitalize w-[162px] md:w-auto'
+                >
+                  {cat}
+                </TabsTrigger>
+              )
+            })}
+          </TabsList>
+          {/* tabs content */}
+          <div className='text-lg xl:mt-8 grid grid-cols-1 lg:grid-cols-3 gap-4'>
+            {filteredProjects.map((project, i)=> {
+              return(
+                <TabsContent value={category} key={i}>
+                  <ProjectCard project={project} />
+                </TabsContent>
+              )
+            })}
+          </div>
+        </Tabs>
+      </div>
+    </section>
   )
 }
 
---
+++ components/DummyData.jsx
@@ -0,0 +1,58 @@
+export const projectData = [
+    {
+        image:'/work/3.png',
+        category: 'react js',
+        name: 'Nexa Website',
+        description: 'NexaLorem ipsum dolor sit amet consectetur adipisicing elit',
+        link: '/',
+        github: '/'
+    },
+    {
+        image:'/work/2.png',
+        category: 'next js',
+        name: 'Udin Website',
+        description: 'Udin Lorem ipsum dolor sit amet consectetur adipisicing elit',
+        link: '/',
+        github: '/'
+    },
+    {
+        image:'/work/1.png',
+        category: 'next js',
+        name: 'Kamu nanya Website',
+        description: 'Kamu nanya Lorem ipsum dolor sit amet consectetur adipisicing elit',
+        link: '/',
+        github: '/'
+    },
+    {
+        image:'/work/4.png',
+        category: 'react native',
+        name: 'Mobilio honda nih',
+        description: 'Mobilio Lorem ipsum dolor sit amet consectetur adipisicing elit',
+        link: '/',
+        github: '/'
+    },
+    {
+        image:'/work/3.png',
+        category: 'react native',
+        name: 'Bayy mobile',
+        description: 'Bayy Lorem ipsum dolor sit amet consectetur adipisicing elit',
+        link: '/',
+        github: '/'
+    },
+    {
+        image:'/work/1.png',
+        category: 'react js',
+        name: 'Becanda aja website',
+        description: 'Becyandaa Lorem ipsum dolor sit amet consectetur adipisicing elit',
+        link: '/',
+        github: '/'
+    },
+    {
+        image:'/work/1.png',
+        category: 'react js',
+        name: 'Test aja sih ini mah',
+        description: 'Test aja Lorem ipsum dolor sit amet consectetur adipisicing elit',
+        link: '/',
+        github: '/'
+    },
+]
